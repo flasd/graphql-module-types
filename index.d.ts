1b5402f99b6aaf214527fd00b01177df58113ff9
Marcel Coelho <flasd2@gmail.com>
2020-10-10 13:32:14 -0300

    fix: move to default query export

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,12 @@
 
 declare module "*.gql" {
   import { DocumentNode } from "graphql";
-  const query: { readonly [key: string]: DocumentNode };
+  const query: DocumentNode;
   export default query;
 }
 
 declare module "*.graphql" {
   import { DocumentNode } from "graphql";
-  const query: { readonly [key: string]: DocumentNode };
+  const query: DocumentNode;
   export default query;
 }
